9122dfc5001e0fd745976ba72a87c37bdbdf9198
yan358941877 <yan358941877@163.com>
2017-07-14 21:08:47 +0800

    给note增加时间监听

--- src/js/module/Note.js
+++ src/js/module/Note.js
@@ -14,15 +14,28 @@ Note.prototype = {
     var date = new Date()
     var $noteItem = $('<div class="note-item">'
                   + '<div class="note-tape"></div>'
-                  + '<div class="note-content"></div>'
-                  + '<div class="note-info">'
+                  + '</div>')
+    var $closeBtn = $('<span><i class="iconfont icon-close"></i><span>')
+    $closeBtn.on("click", function(){
+      // 发送一个消息，删除对应的note
+    })
+    var $noteContent = $('<div class="note-content" contenteditable="plaintext-only"></div>')
+    $noteContent.text('请输入内容')
+    $noteContent.on('blur', function(){
+      //alert('结束编辑')
+    })
+    $noteContent.on('focus', function(){
+      //alert('开始编辑')
+    })
+    var $noteInfo = $('<div class="note-info">'
                   + '<p>' + this.id + '</p>'
                   + '<p>' + date.toLocaleString() + '</p>'
-                  + '</div></div>')
-    var $closeBtn = $('<span><i class="iconfont icon-close"></i><span>')
-    $noteItem.prepend($closeBtn)
+                  + '</div>')
+    $noteItem.append($closeBtn)
+    $noteItem.append($noteContent)
+    $noteItem.append($noteInfo)
     this.$element.html($noteItem)
-    this.$element.height(Math.floor(Math.random()*300))
+    //this.$element.height(Math.floor(Math.random()*300))
     this.$container.append(this.$element)
   }
 }
